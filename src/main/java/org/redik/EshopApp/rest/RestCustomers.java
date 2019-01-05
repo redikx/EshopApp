b4ad36096ae191f860f8674fa8c5aa2af6ed3859
redik <redikx@gmail.com>
2019-01-05 23:40:21 +0100

    Add first RestApi EshopApp/api/allCustomers : run OK in Tomcat modify log4.properties to get rid errors

--- src/main/java/org/redik/EshopApp/rest/RestCustomers.java
+++ src/main/java/org/redik/EshopApp/rest/RestCustomers.java
@@ -1,7 +1,23 @@
 package org.redik.EshopApp.rest;
 
+import java.util.List;
 
+import org.redik.EshopApp.entity.Customer;
+import org.redik.EshopApp.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api")
 public class RestCustomers {
 
+	@Autowired
+	CustomerService customerService;
+	
+	@GetMapping("/allCustomers")
+	public List<Customer> RestGetAllCustomers() {
+		  return customerService.getAllCustomer();
+	}
 }
