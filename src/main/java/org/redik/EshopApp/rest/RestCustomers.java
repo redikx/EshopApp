c0f02bedd4e5a1ed2e11336c42a3a00c4529587e
redik <redikx@gmail.com>
2019-01-08 22:38:15 +0100

    add Rest Api to get single customerId : eg http://localhost:8080/EshopApp/api/getCustomer?customerId=11

--- src/main/java/org/redik/EshopApp/rest/RestCustomers.java
+++ src/main/java/org/redik/EshopApp/rest/RestCustomers.java
@@ -7,6 +7,7 @@ import org.redik.EshopApp.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,4 +21,10 @@ public class RestCustomers {
 	public List<Customer> RestGetAllCustomers() {
 		  return customerService.getAllCustomer();
 	}
+	
+	@GetMapping("/getCustomer")
+	public Customer getCustomer(@RequestParam("customerId") int customerId) {
+		return customerService.getCustomer(customerId);
+	}
+	
 }
